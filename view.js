3425c7b46b6929bc9f80df7265d3c5e16b8381ba
aposte7 <olman4jesus@gmail.com>
2025-02-21 19:10:10 +0300

    add game board and settings UI components

--- view.js
+++ view.js
@@ -1,7 +1,63 @@
 export function GameUI(element) {
 	element.innerHTML = `
   <div class="game_container">
-     <h1>Welcome</h1>
+      <div class="board">
+          <div data-cell-row-1 class="board_row">
+              <button data-board-col-1 class="board_cell">
+                X
+              </button>
+
+              <button data-board-col-2 class="board_cell">
+                O
+              </button>
+
+              <button data-board-col-3 class="board_cell">
+              </button>
+          </div>
+          
+          <div data-cell-row-2 class="board_row">
+              <button data-board-col-1 class="board_cell">
+              </button>
+
+              <button data-board-col-2 class="board_cell">
+              </button>
+
+              <button data-board-col-3 class="board_cell">
+              </button>
+          </div>
+
+          <div data-cell-row-3 class="board_row">
+              <button data-board-col-1 class="board_cell">
+              </button>
+
+              <button data-board-col-2 class="board_cell">
+              </button>
+
+              <button data-board-col-3 class="board_cell">
+              </button>
+          </div>
+      </div>
+
+      <div class="game_setting">
+          <div class="game_setting_wrapper">
+              <p>
+                Choose Opponent
+                <span>Player</span>
+              </p>
+
+              <div class="game_setting_opponents">
+                <button data-player-1>AI</button>
+                <button data-player-2>Human</button>
+              </div>
+          </div>
+
+          <div class="symbol">
+            <p>You <span>X</span></p>
+            <p>Player-2 <span>O</span></p>
+          </div>
+
+          <div class="level"></div>
+      </div>
   </div>
   
   `
